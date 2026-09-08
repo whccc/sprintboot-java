f46fe30e3caa69ed09675c1166ed6f625d871c30
wilson <wilson@gmail.com>
2026-09-08 07:49:43 -0500

    base

---
+++ src/main/java/com/practica/crudpruebas/CrudpruebasApplication.java
@@ -0,0 +1,13 @@
+package com.practica.crudpruebas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudpruebasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudpruebasApplication.class, args);
+	}
+
+}
